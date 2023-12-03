0c9fe5c7b3214b55d21a5de8afafeee273d04c47
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-12-03 00:12:21 +0000

    feat: add verify_dockerised function

--- helper-functions.sh
+++ helper-functions.sh
@@ -76,3 +76,25 @@ verify_dependency() {
         return 1
     fi
 }
+
+verify_dockerised() {
+
+    # This is a very commonly used method to verify that you are
+    # within a docker container. However, the .dockerenv file is
+    # not intended to be used this way. Despite this, it seems to
+    # be left untouched, likely because of this fact.
+    #
+    # See: https://github.com/moby/moby/issues/18355#issuecomment-220484748
+
+    if [ -f /.dockerenv ]; then
+        return 0
+    fi
+
+    # As the above method may be fragile, let's also check here.
+    
+    if grep -qE '/docker/' /proc/self/cgroup; then
+        return 0
+    fi
+
+    return 1
+}
